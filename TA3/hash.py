fd3ba0a2a5c28bc079533aa2cc93210fd541e420
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-11-22 14:38:19 +0530

    Delete TA3 directory

--- TA3/hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def generateHash(input_string):
-    hash_object = hashlib.sha256()
-    hash_object.update(input_string.encode('utf-8'))
-    hash_value = hash_object.hexdigest()
-    return hash_value
-
